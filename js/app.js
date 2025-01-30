5ae49e382b138f85986acd2fd1bd0fb11448b5f9
giusmili <giusmili67@gmail.com>
2025-01-30 12:15:15 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
 (() => {
-    let firstName, lastName, age, tabUser;
 
-    firstName = String(window.prompt("Votre nom s'il plait?", ""));
-    lastName = String(prompt("Votre prénom s'il plait?", ""));
-    age = Number(parseInt(prompt("Votre age s'il plait?", "")));
+    const btn = document.querySelector("section .button");
+    const printout = document.querySelector("section h2");
 
-    tabUser = [];
-    tabUser.push(firstName, lastName, age);
+    btn.addEventListener("click", e => {
+        e.stopPropagation();
+        //alert(e.target.tagName)
+        callUser.dialog();
+    });
 
-    const User = {
-        stock: tabUser
+    const callUser = {
+
+        dialog() {
+
+            firstName = String(window.prompt("Votre nom s'il plait?", ""));
+            lastName = String(prompt("Votre prénom s'il plait?", ""));
+            age = Number(parseInt(prompt("Votre age s'il plait?", "")));
+
+            tabUser = [];
+            tabUser.push(firstName, lastName, age);
+
+            const User = {
+                stock: tabUser
+            };
+            Object.seal(User);
+            console.table(tabUser);
+
+            /* localStorage */
+            localStorage.setItem("user", JSON.stringify(User.stock));
+            let unstock = localStorage.getItem("user");
+            let userIndex = JSON.parse(unstock);
+            printout.innerHTML = `Bonjour ${userIndex.join(' / ')}`;
+        }
     };
-    Object.seal(User);
-    console.table(tabUser);
 })();
--- src/app.js
+++ src/app.js
@@ -1,20 +1,44 @@
 (()=>{
-    let firstName, 
-        lastName, 
-        age,
-        tabUser
     
-    firstName = String(window.prompt("Votre nom s'il plait?",""))
-    lastName = String(prompt("Votre prénom s'il plait?",""))
-    age = Number(parseInt(prompt("Votre age s'il plait?","")))
 
-    tabUser = []
-    tabUser.push(firstName, lastName, age)
+    const btn = document.querySelector("section .button")
+    const printout = document.querySelector("section h2")
+    
+    btn.addEventListener("click", (e)=>{
+        e.stopPropagation()
+        //alert(e.target.tagName)
+        callUser.dialog()
+    })
+    
+    const callUser = {
+        
+        dialog(){
+           
+            firstName = String(window.prompt("Votre nom s'il plait?",""))
+            lastName = String(prompt("Votre prénom s'il plait?",""))
+            age = Number(parseInt(prompt("Votre age s'il plait?","")))
+           
 
-    const User = {
-        stock: tabUser
-    }
-    Object.seal(User)
-    console.table(tabUser)
+            tabUser = []
+            tabUser.push(firstName, lastName, age)
+    
+
+            const User = {
+                stock: tabUser
+            }
+            Object.seal(User)
+            console.table(tabUser)
+
+            /* localStorage */
+            localStorage.setItem("user", JSON.stringify(User.stock))
+            let unstock = localStorage.getItem("user")
+            let userIndex = JSON.parse(unstock)
+            printout.innerHTML = `Bonjour ${userIndex.join(' / ')}`
+
+        }
+    }       
+
+    
+   
 
 })()
