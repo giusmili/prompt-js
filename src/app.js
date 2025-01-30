447e5005cedc3da58ec4e05bebace175056d0f4c
giusmili <giusmili67@gmail.com>
2025-01-30 10:44:21 +0100

    js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+(()=>{
+    let firstName, 
+        lastName, 
+        age,
+        tabUser
+    
+    firstName = String(prompt("Votre s'il plait?",""))
+
+    tabUser = []
+    tabUser.push(firstName)
+
+    console.table(tabUser)
+
+})()
