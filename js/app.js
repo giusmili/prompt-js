4bd6f2b094be9a008449b832277c4faa8564db2b
giusmili <giusmili67@gmail.com>
2025-01-30 11:07:08 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 (() => {
     let firstName, lastName, age, tabUser;
 
-    firstName = String(prompt("Votre s'il plait?", ""));
+    firstName = String(window.prompt("Votre nom s'il plait?", ""));
+    lastName = String(prompt("Votre prénom s'il plait?", ""));
+    age = Number(parseInt(prompt("Votre age s'il plait?", "")));
 
     tabUser = [];
-    tabUser.push(firstName);
+    tabUser.push(firstName, lastName, age);
 
+    const User = {
+        stock: tabUser
+    };
+    Object.seal(User);
     console.table(tabUser);
 })();
