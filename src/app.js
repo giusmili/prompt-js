c64e2f430745e8957a19deb9f6209944976417d9
giusmili <giusmili67@gmail.com>
2025-02-06 10:39:52 +0100

    js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 
     const btn = document.querySelector("section .button")
     const printout = document.querySelector("section h2")
+    let firstName, lastName, age
     
     btn.addEventListener("click", (e)=>{
         e.stopPropagation()
@@ -17,6 +18,27 @@
             firstName = String(window.prompt("Votre nom s'il vous plait?",""))
             lastName = String(prompt("Votre prénom s'il vous plait?",""))
             age = Number(parseInt(prompt("Votre age s'il vous plait?","")))
+
+            if(firstName && lastName && age){
+              /*   Swal.fire({
+                    title: "Bon travail", 
+                    text: "You clicked the button!",
+                    icon: "success"
+                  }); */
+                printout.classList.remove("warning")
+                printout.innerHTML = `${firstName} ${lastName} ${age}`
+            }
+            else{
+                /* Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Something went wrong!",
+                    footer: '<a href="#">Why do I have this issue?</a>'
+                  }); */
+                   printout.classList.add("warning")
+                    printout.innerText = "Tous les champs sont obligatores!!!"
+                  
+            }
            
 
             tabUser = []
@@ -33,7 +55,9 @@
             localStorage.setItem("user", JSON.stringify(User.stock))
             let unstock = localStorage.getItem("user")
             let userIndex = JSON.parse(unstock)
-            printout.innerHTML = `Bonjour ${userIndex.join(' / ')}`
+            
+           
+        
 
         }
     }       
